d123ec79714769b4680fbb809a077ebd7ca61bed
Tyler Garlick <tjgarlick@gmail.com>
2014-01-24 14:57:37 -0500

    Broke apart some of the interfaces and mapped to ioc container

---
+++ FlitBit.Dapper/FlitBit.Dapper/Infrastructure/Dto/IConstructorFinder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlitBit.Dapper.Infrastructure.Dto
+{
+    
+    public interface IConstructorFinder
+    {
+        ConstructorInfo GetConstructor(Type type);
+    }
+
+    public class DefaultConstructorFinder
+    {
+        
+    }
+}
